a88f739a0ed4c5ed7eb2de5b1cd96f88e861e9cb
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-12 23:04:48 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+dictionary = {"hello" => "hi ", "to, two, too" => '2',"for, four"  => '4',
+'be'  => 'b',
+'you'  => 'u',
+"at"  => "@",
+"and"  => "&"}
+
+def word_substituter(str)
+  dictionary = {"hello" => "hi ", "to" => 2, "two" => 2, "too" => '2',"for, four"  => '4',
+  'be'  => 'b',
+  'you'  => 'u',
+  "at"  => "@",
+  "and"  => "&"}
+  arr = str.split(" ")
+  new_arr = []
+  arr.each do |word|
+    if dictionary.keys.include?(word)
+      new_arr << dictionary[word]
+    else
+      new_arr << word
+    end
+  end
+  new_arr.join(" ")
+end
