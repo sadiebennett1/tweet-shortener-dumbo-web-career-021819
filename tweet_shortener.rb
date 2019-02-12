c384618c79c1521ee1c8f85c23ae30c5c693cfcc
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-12 23:10:30 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 
 def word_substituter(str)
-  dictionary = {"hello" => "hi ", "to" => 2, "two" => 2, "too" => '2',"for" => 4, "four"  => 4, "For" => 4
+  dictionary = {"hello" => "hi ", "to" => 2, "two" => 2, "too" => 2,"for" => 4, "four"  => 4, "For" => 4
   'be'  => 'b',
   'you'  => 'u',
   "at"  => "@",
