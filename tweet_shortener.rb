845e6f7dc8fbfec9da9206506718a9786e273a31
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-12 23:06:12 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,9 +1,4 @@
 # Write your code here.
-dictionary = {"hello" => "hi ", "to, two, too" => '2',"for, four"  => '4',
-'be'  => 'b',
-'you'  => 'u',
-"at"  => "@",
-"and"  => "&"}
 
 def word_substituter(str)
   dictionary = {"hello" => "hi ", "to" => 2, "two" => 2, "too" => '2',"for, four"  => '4',
